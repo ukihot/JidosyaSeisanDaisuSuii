55939dd2e4646bf3782a06397850a60c80123477
TOKUNAGA <82358857+ukihot@users.noreply.github.com>
2021-04-20 16:11:24 +0900

    wip

--- src/AutomobileProductionScraping.py
+++ src/AutomobileProductionScraping.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
 #coding: UTF-8
+import csv
+import io
 import os
-import sys
 import re
+import shutil
+import sys
+import urllib
+from tkinter import messagebox
+from urllib.request import urlopen
+
+import MST_MAKER_URL
+import pandas as pd
 import requests
+import tabula
+import xlrd
+from bs4 import BeautifulSoup
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
-import xlrd
-import pandas as pd
-from tkinter import messagebox
-import urllib
-import tabula
-import csv
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import const
-import shutil
 
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+values = {'name': 'Michael Foord',
+              'location': 'Northampton',
+              'language': 'Python' }
+data = urllib.parse.urlencode(values).encode('utf-8')
 def main():
     messagebox.showinfo('確認', '各メーカーの自動車生産台数を現在のフォルダ下に出力します。')
     if not os.path.exists('data'):
@@ -24,17 +31,31 @@ def main():
     if not os.path.exists('out'):
         os.mkdir('out')
     seisan_daisu = [0] * 11
-    seisan_daisu[0] = xl_scraping(const.tyturl, '生産', 2, 6, 202101.0, "data/toyota.xls")
-    seisan_daisu[1] = xl_scraping(const.tyturl, '生産', 2, 13, 202101.0, "data/toyota.xls")
-    seisan_daisu[2] = xl_scraping(const.hndurl, '日本語', 84, 85, '1月実績', "data/honda.xlsx")
-    seisan_daisu[4] = web_scraping(const.szkurl, 2, 1, "data/suzuki.csv")
-    seisan_daisu[5] = web_scraping(const.mzdurl, 4, 2, "data/mazda.csv")
-    seisan_daisu[6] = web_scraping(const.mtburl, 4, 2, "data/mitsubishi.csv")
-    seisan_daisu[8] = web_scraping(const.iszurl, 0, 11, "data/isuzu.csv")
-    seisan_daisu[9] = xl_scraping(const.tyturl, '生産', 2, 20, 202101.0, "data/toyota.xls")
-    seisan_daisu[10] = web_scraping(const.hsourl, 0, 11, "data/huso.csv")
+    seisan_daisu[0] = xl_scraping(MST_MAKER_URL.tyturl, '生産', 2, 6, 202101.0, "data/toyota.xls")
+    seisan_daisu[1] = xl_scraping(MST_MAKER_URL.tyturl, '生産', 2, 13, 202101.0, "data/toyota.xls")
+    seisan_daisu[2] = xl_scraping(MST_MAKER_URL.hndurl, '日本語', 84, 85, '1月実績', "data/honda.xlsx")
+    seisan_daisu[4] = web_scraping(MST_MAKER_URL.szkurl, 2, 1, "data/suzuki.csv")
+    seisan_daisu[5] = web_scraping(MST_MAKER_URL.mzdurl, 4, 2, "data/mazda.csv")
+    seisan_daisu[6] = web_scraping(MST_MAKER_URL.mtburl, 4, 2, "data/mitsubishi.csv")
+    seisan_daisu[8] = web_scraping(MST_MAKER_URL.iszurl, 0, 11, "data/isuzu.csv")
+    seisan_daisu[9] = xl_scraping(MST_MAKER_URL.tyturl, '生産', 2, 20, 202101.0, "data/toyota.xls")
+    seisan_daisu[10] = web_scraping(MST_MAKER_URL.hsourl, 0, 11, "data/huso.csv")
     output_excle(seisan_daisu)
     shutil.rmtree('data')
+
+# マスタ値更新
+def updata_mst_maker_url():
+    req = urllib.request.Request(MST_MAKER_URL.prt_mzd_url, data, headers)
+    html = urllib.request.urlopen(req)
+    soup = BeautifulSoup(html, "html.parser")
+    elems = soup.select(MST_MAKER_URL.select_wd_mzd)
+
+    ls_url = []
+    for elem in elems:
+        ls_url.append(elem.get('href')) if (extraction := re.search('2021年[1-12]月の生産', elem.getText())) else None
+    print(ls_url)
+    for (index, url) in enumerate(ls_url):
+        ls_url[index] = re.search(r'/.*l', url).group(0)
 
 # データ出力
 def output_excle(seisan_daisu):
@@ -50,6 +71,7 @@ def output_excle(seisan_daisu):
             ws.cell(row=row_no, column=col_no, value=value)
     wb.save('out/集計結果.xlsx')
 
+# エクセルスクレイピング
 def xl_scraping(url, sheet_name, row, target_row, cell_name, FILEPATH):
     r = requests.get(url, allow_redirects=True)
     open(FILEPATH, 'wb').write(r.content)
@@ -59,13 +81,8 @@ def xl_scraping(url, sheet_name, row, target_row, cell_name, FILEPATH):
         if(cell.value == cell_name):
             return st.cell_value(target_row, col_j)
 
+# Webスクレイピング
 def web_scraping(url, r, c,FILEPATH):
-    # URLの指定
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
-    values = {'name': 'Michael Foord',
-          'location': 'Northampton',
-          'language': 'Python' }
-    data = urllib.parse.urlencode(values).encode('utf-8')
     req = urllib.request.Request(url, data, headers)
     try:
         html = urllib.request.urlopen(req)
--- src/MST_MAKER_URL.py
+++ src/MST_MAKER_URL.py
@@ -1,7 +1,12 @@
+# 各メーカーのURL
 mzdurl = 'https://newsroom.mazda.com/ja/publicity/release/2021/202102/210225a.html'
 szkurl = 'https://www.suzuki.co.jp/release/d/2021/0225/'
 mtburl = 'https://www.mitsubishi-motors.com/jp/newsrelease/2021/detail5509.html'
 iszurl = 'http://www.jada.or.jp/y-r-maker-isuzu'
 hsourl = 'http://www.jada.or.jp/data/year/y-r-hanbai/y-r-maker/y-r-maker-mitsubishi-fuso/'
 tyturl = 'https://global.toyota/pages/global_toyota/company/profile/production-sales-figures/production_sales_figures_jp.xls'
-hndurl ='https://www.honda.co.jp/content/dam/site/www/investors/cq_img/financial_data/monthly/CY2020_202102_monthly_data_j.xlsx'
+hndurl = 'https://www.honda.co.jp/content/dam/site/www/investors/cq_img/financial_data/monthly/CY2020_202102_monthly_data_j.xlsx'
+
+# 更新用の情報
+prt_mzd_url = 'https://newsroom.mazda.com/ja/publicity/release/2021/'
+home_mzd ='https://newsroom.mazda.com/'
